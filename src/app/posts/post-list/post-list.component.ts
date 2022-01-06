bb07c6a583fbd10bc10b3e73bee8caaffdb5e74c
Hicham <hicham.labrahimi@googlemail.com>
2022-01-06 14:58:30 +0100

    Keine Ahnung

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,12 +1,12 @@
 import { Component, Input } from '@angular/core';
-
+import { Post } from '../post.model';
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css'],
 })
 export class PostListComponent {
-  @Input() posts: { title: string, content: string }[] = [];
+  @Input() posts: Post[] = [];
   /* posts = [
     { title: 'First Post', content: "The first post's content" },
     { title: 'Second Post', content: "The second post's content" },
