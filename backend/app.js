861de73813445d877741d9821c0a62e5cf848a5a
Hicham <hicham.labrahimi@googlemail.com>
2022-03-22 15:51:26 +0100

    Implement delete function

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 
 const Post = require('./models/post');
 const { retryWhen } = require('rxjs');
+const { createShorthandPropertyAssignment, createPostfix } = require('typescript');
 
 const app = express();
 
@@ -30,10 +31,14 @@ app.post("/api/posts", (req, res, next)=>{
     title: req.body.title,
     content: req.body.content
   });
-  post.save();
-  res.status(201).json({
-    message: "Post added MF"
-  });
+  post.save()
+    .then(createdPost => {
+      res.status(201).json({
+        message: "Post added MF",
+        postId: createdPost._id
+      });
+    });
+
 });
 
 app.get('/api/posts', (req, res, next) => {
@@ -47,7 +52,10 @@ app.get('/api/posts', (req, res, next) => {
 });
 
 app.delete("/api/posts/:id", (req, res, next) => {
-  console.log(req.params.id);
+  Post.deleteOne({_id: req.params.id})
+    .then(result => {
+      console.log(result);
+    });
   res.status(200).json({message: "Post deleted!"});
 });
 
